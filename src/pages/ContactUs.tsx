9cbd676f056fc8d326493e1fe7db042a3f49c6f4
NachoXS-NET <pantojaignacio642@gmail.com>
2024-08-08 13:33:51 -0300

    creating contact-us and updating routing

---
+++ src/pages/ContactUs.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+
+export default function ContactUs() {
+  return (
+    <>
+      <Layout>
+        <section className="w-full h-max">
+          <div className="mb-8 text-center">
+            <h2 className="text-bluemain text-center text-4xl font-extrabold">DESTINOS RECOMENDADOS</h2>
+            <hr className='w-2/5 m-auto border text-bluesec text-center mt-4' />
+          </div>
+        </section>
+      </Layout >
+    </>
+  )
+}
