e9bb6b3b130b4cc876d771a1a219760c3c570f9c
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-08-27 11:46:43 -0300

    fixing bugs

--- src/config/api/getPackages.tsx
+++ src/config/api/getPackages.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import {PackageProps, categoryProp} from "../types";
+import { PackageProps, categoryProp } from "../types";
 import { getFirestore, getDocs, collection } from "firebase/firestore";
 import Card from "../../components/Card";
 import Loader from "../../components/ui/loaders/Loader";
@@ -7,6 +7,7 @@ import Loader from "../../components/ui/loaders/Loader";
 
 export default function GetPackages({ categoryProp }: categoryProp) {
   const [packages, setPackages] = useState<PackageProps[]>([]);
+  const [noPackages, setNoPackages] = useState(false)
   const filteredCategory = categoryProp === "Todas" ? packages : packages.filter(pack => pack.categoria === categoryProp)
   const db = getFirestore();
   const fetchData = async () => {
@@ -24,7 +25,21 @@ export default function GetPackages({ categoryProp }: categoryProp) {
 
   useEffect(() => {
     fetchData();
+    const timer = setTimeout(() => {
+      if (packages.length === 0) {
+        setNoPackages(true)
+      }
+    }, 5000);
+  
+    return () => clearTimeout(timer)
   }, []);
+
+  useEffect(() => {
+    if (packages.length > 0) {
+      setNoPackages(false);
+    }
+  }, [packages]);
+
 
   return (
     <>
@@ -32,6 +47,10 @@ export default function GetPackages({ categoryProp }: categoryProp) {
         filteredCategory.map((pkg) => (
           <Card key={pkg.id} id={pkg.id} isPromoted={pkg.isPromoted} salida={pkg.salida} destino={pkg.destino} imgUrl={pkg.imgUrl} categoria={pkg.categoria} />
         ))
+      ) : noPackages ? (
+        <div className="w-full h-full flex items-center justify-center">
+          <p className="text-2xl">No hay paquetes</p>
+        </div>
       ) : (
         <div className="w-full h-full flex items-center justify-center">
           <Loader />
