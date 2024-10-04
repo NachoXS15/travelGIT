e9921b9204230c632793009a9a0f8c9fca40d778
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-10-04 10:32:32 -0300

    created search bar for admin

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import HeaderAdmin from "../../components/HeaderAdmin";
 import { PackageProps } from "../../config/types";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import CategoryTag from "../../components/ui/CategoryTag";
-import { Edit, Trash } from "../../components/ui/Icons";
+import { Cancel, Edit, Search, Trash } from "../../components/ui/Icons";
 import Loader from "../../components/ui/loaders/Loader";
 import PromotedTag from "../../components/ui/PromotedTag";
 import CreatePackage from "./CreatePackage";
@@ -14,9 +14,9 @@ import EditPackage from "./EditPackage";
 
 export default function Dashboard() {
   const [packages, setPackages] = useState<PackageProps[]>([])
-  // const [search, setSearch] = useState<string | undefined>("")
-  // console.log(search);
-  // const filteredPackages = search ? packages.filter(pkg => pkg.destino.toLowerCase().includes(search.toLowerCase())) : packages;
+  const [search, setSearch] = useState<string | undefined>("")
+  console.log(search);
+  const filteredPackages = search ? packages.filter(pkg => pkg.destino.toLowerCase().includes(search.toLowerCase())) : packages;
   const [createModalOpen, setCreateModalOpen] = useState(false);
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
@@ -58,21 +58,21 @@ export default function Dashboard() {
 
   useEffect(() => {
     fetchData();
-  }, [packages]);
+  }, []);
 
 
-  // const form = useRef<HTMLFormElement>(null);
+  const form = useRef<HTMLFormElement>(null);
 
-  // const handleSearch = (e: React.FormEvent) => {
-  //   e.preventDefault();
-	// 	const formData = new FormData(form.current!);
-  //   const searchData = formData.get("search")?.toString()
-  //   setSearch(searchData)
-  // }
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+		const formData = new FormData(form.current!);
+    const searchData = formData.get("search")?.toString()
+    setSearch(searchData)
+  }
 
-  // const clearSearch = () => {
-  //   setSearch("");
-  // }
+  const clearSearch = () => {
+    setSearch("");
+  }
 
   // const auth = getAuth();
   // useEffect(() => {
@@ -92,20 +92,20 @@ export default function Dashboard() {
     <>
       <HeaderAdmin />
       <div className="w-full my-7 px-5 md:w-4/5 m-auto flex items-center flex-col" style={{ fontFamily: "Mundial" }}>
-        <div className="w-full flex md:flex-row justify-between">
+        <div className="w-full flex flex-col md:flex-row justify-between">
           <h2 className="text-bluemain text-2xl md:text-3xl my-3 font-semibold">Lista de paquetes</h2>
-          {/* <div className="flex justify-between px-3 items-center md:gap-5">
+          <div className="flex justify-between px-3 items-center md:gap-5">
             <form ref={form} onSubmit={handleSearch} className="flex items-center">
-              <input type="text" className="w-56 border-b border-bluemain focus:outline-none" placeholder="Buscar" />
+              <input type="text" name="search" className="w-56 border-b border-bluemain focus:outline-none" placeholder="Buscar" />
               <button type="submit"><Search/></button>
               <button type="reset" onClick={clearSearch}><Cancel /></button>
             </form>
-          </div> */}
-          <button className="text-4xl text-bluemain hover:scale-110 transition" onClick={openCreateModal}>+</button>
+            <button className="text-4xl text-bluemain hover:scale-110 transition" onClick={openCreateModal}>+</button>
+          </div>
         </div>
         <div className="w-full flex flex-col gap-5">
           {
-            packages && packages.length > 0 ? packages.map((pkg, index) => (
+            filteredPackages && filteredPackages.length > 0 ? filteredPackages.map((pkg, index) => (
               <div key={index} className="w-full py-4 px-7 rounded-md md:rounded-2xl bg-lightgray shadow-lg flex flex-col md:flex-row justify-between gap-10">
                 <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
                   <img src={pkg.imgUrl[0]} className="w-full md:w-52 rounded-lg" alt="" />
