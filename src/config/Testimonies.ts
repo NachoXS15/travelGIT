6d41c11e378895eaf544bd9d8a96175e98bca4f7
NachoXS-NET <pantojaignacio642@gmail.com>
2024-11-14 21:41:21 -0300

    created testimonies file

---
+++ src/config/Testimonies.ts
@@ -0,0 +1,28 @@
+const Testimonies = [
+    {
+        "passenger": "Juan Carlos Peralta",
+        "msg": "Excelente el servicio de turismo que ofrecen, con ellos realizamos 3 excursiones y muy buenas, y el agregado de los choferes guías Jorge y Alberto de muy buena predisposición en lo uno requería, es recomendable todo los paquetes que ofrecen."
+    },
+    {
+        "passenger": "Susana Dicapua",
+        "msg": "Hicimos excursión a Valle de la Luna y Talampaya con esa empresa, muy puntuales, atención excelente y buen costo, gracias Romina y Daniel."
+    },
+    {
+        "passenger": "Yani Allevato",
+        "msg": "Súper recomendable, Muy buena Atención desde el primer momento en que nos contactamos. Higinia muy amable y dispuesta. Mario, nuestro guía, excelente!! Realizamos la excursión del Talampaya y Valle de la Luna junto a el, fue muy servicial y nos brindó de buena información. Una experiencia inolvidable."
+    },
+    {
+        "passenger": "Federico Ardiles",
+        "msg": "Fui a conocer la provincia por cinco días, realicé dos excursiones con ellos, muy buen asesoramiento por parte de Higinia y de Manuel, los guías que me tocaron fueron un día Ivan y al otro día Alberto, buena predisposición y conocimiento de los lugares por parte de ellos, recomendable y muy buen servicio el ofrecido!!"
+    },
+    {
+        "passenger": "Analía Talamo",
+        "msg": "Realizamos la excursión a Chilecito y cuesta de Miranda, salimos de noche y volvimos de noche pero valió la pena. Muy recomendable, hermosos lugares y los guías Walter y Poli excelentes!!!"
+    },
+    {
+        "passenger": "Ezequiel Strawich",
+        "msg": "Muy bien servicio, te ofrecen lo mejor de La Rioja lo recomiendo lo mejor la Laguna Brava."
+    },
+]
+
+export default Testimonies
