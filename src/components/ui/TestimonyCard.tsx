ad7a809f57073be01186430cd2fdc64bf809e601
NachoXS-NET <pantojaignacio642@gmail.com>
2024-11-14 22:25:15 -0300

    testimonies card added

---
+++ src/components/ui/TestimonyCard.tsx
@@ -0,0 +1,13 @@
+type testProps = {
+    passenger: string
+    msg: string
+}
+
+export default function TestimonyCard({msg, passenger}: testProps) {
+  return (
+    <div className="w-1/3 h-fit bg-white shadow-md rounded-md p-10" style={{fontFamily: 'Mundial'}}>
+        <h2 className="text-bluesec font-bold text-2xl mb-2">{passenger}</h2>
+        <p className="leading-relaxed">{msg}</p>
+    </div>
+  )
+}
--- src/pages/Testimony.tsx
+++ src/pages/Testimony.tsx
@@ -1,13 +1,21 @@
 import Layout from "../components/Layout";
-
+import TestimonyCard from "../components/ui/TestimonyCard";
+import Testimonies from "../config/Testimonies";
 export default function Testimony() {
   return (
     <Layout>
-        <section className="w-full h-[350px] bg-cover bg-center"
+      <section className="w-full h-[350px] bg-cover bg-center"
         style={{ backgroundImage: `url("https://www.10wallpaper.com/wallpaper/1920x1080/1410/Best_Nature_Views-Scenery_HD_Wallpaper_1920x1080.jpg")` }}>
         <div className="w-full h-full flex justify-center items-center bg-bluesec bg-opacity-70">
           <p className="text-white text-4xl font-semibold md:text-7xl" style={{ fontFamily: 'Mundial' }}>TESTIMONIOS</p>
         </div>
+      </section>
+      <section className="w-full md:w-full h-fit flex flex-wrap justify-center gap-10 my-10">
+        {
+          Testimonies.map(test => (
+            <TestimonyCard passenger={test.passenger} msg={test.msg} />
+          ))
+        }
       </section>
     </Layout>
   )
