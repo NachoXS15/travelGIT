5f56886a410a235847d33350d2b4fe957134551b
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-11-16 17:14:53 -0300

    replaced banners

--- src/pages/Testimony.tsx
+++ src/pages/Testimony.tsx
@@ -6,7 +6,7 @@ export default function Testimony() {
   return (
     <Layout>
       <section className="w-full h-[350px] bg-cover bg-center"
-        style={{ backgroundImage: `url("https://www.10wallpaper.com/wallpaper/1920x1080/1410/Best_Nature_Views-Scenery_HD_Wallpaper_1920x1080.jpg")` }}>
+        style={{ backgroundImage: `url("https://tourtravelandmore.com/wp-content/uploads/2021/10/image-6.jpeg")` }}>
         <div className="w-full h-full flex justify-center items-center bg-bluesec bg-opacity-70">
           <p className="text-white text-4xl font-semibold md:text-7xl" style={{ fontFamily: 'Mundial' }}>TESTIMONIOS</p>
         </div>
